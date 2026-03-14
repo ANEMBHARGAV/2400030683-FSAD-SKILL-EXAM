c88cf0e971cb11a7d67bad77f26aa082985d46a2
ANEM BHARGAV <anembhargav02@gmail.com>
2026-03-14 20:06:20 +0530

    Refactor Department class formatting
    Removed unnecessary whitespace and cleaned up formatting.

--- src/main/java/com/klef/fsad/exam/Department.java
+++ src/main/java/com/klef/fsad/exam/Department.java
@@ -1,27 +1,18 @@
 package com.klef.fsad.exam;
-
 import javax.persistence.*;
 import java.util.Date;
-
 @Entity
 @Table(name="department")
-
 public class Department {
-
  @Id
  @GeneratedValue(strategy = GenerationType.IDENTITY)
  private int id;
-
  private String name;
  private String description;
-
  @Temporal(TemporalType.DATE)
  private Date date;
-
  private String status;
-
  public Department() {}
-
  public Department(String name,String description,Date date,String status)
  {
   this.name=name;
@@ -29,5 +20,4 @@ public class Department {
   this.date=date;
   this.status=status;
  }
-
 }
